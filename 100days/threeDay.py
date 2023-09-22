332e062b567ca0798f9d8a4e74f1d45b8da7a9d3
Infernalloo <infernalloo@proton.me>
2023-09-22 21:03:05 +0200

    did half day 3

---
+++ 100days/threeDay.py
@@ -0,0 +1,15 @@
+# conditional statement
+if True :
+    print("Hello world")
+else :
+    print("Not hello world")
+
+# Comparison operators
+> # Greater than
+< # Less than
+>= # Greater than or equal to
+<= # Less than or equal to
+== # Equal to
+!= # Not equal to
+
+# nested if and elif statements
