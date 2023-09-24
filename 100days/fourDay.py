41cff09872d3709236efdf4f8a34b8b4b1f82e23
Infernalloo <infernalloo@proton.me>
2023-09-24 20:51:57 +0200

    did random module

--- 100days/fourDay.py
+++ 100days/fourDay.py
@@ -1,1 +1,13 @@
 # Randomisation
+import random
+# Import the random module
+random_number = random.randint(1, 10)
+print(random_number)
+# nameOfTheModule.nameOfTheVariable()
+
+random_float = round(random.random() * 5)
+print(random_float)
+
+# Modules
+# Make the program in small modules
+# import the name of the file in the main.py
