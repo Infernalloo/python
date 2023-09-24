a3c9f9af6f6c3295d88c6e521637921a161dcea8
Infernalloo <infernalloo@proton.me>
2023-09-24 16:53:36 +0200

    finished day 3

--- 100days/threeDay.py
+++ 100days/threeDay.py
@@ -13,3 +13,15 @@ else :
 != # Not equal to
 
 # nested if and elif statements
+if True :
+    if True :
+        print("Double True")
+    else :
+        print("False")
+else :
+    print("Not True")
+
+# Logical operators
+# A and B = both need to be true for the statement to be true
+# C or D = only one needs to be true 
+# not E = 
