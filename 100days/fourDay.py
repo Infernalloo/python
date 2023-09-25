a94d1a14477c68086be9a3985660f3fba0ce4f4c
Infernalloo <infernalloo@proton.me>
2023-09-25 10:41:51 +0200

    finished day 4

--- 100days/fourDay.py
+++ 100days/fourDay.py
@@ -7,7 +7,18 @@ print(random_number)
 
 random_float = round(random.random() * 5)
 print(random_float)
+# random.random() returns a number between
+# 0 and 1, not including 1
 
 # Modules
 # Make the program in small modules
 # import the name of the file in the main.py
+
+# Lists
+list = ["item1", "item2", 2, True]
+print(list[3])
+print(list[-4])
+
+list.append() # append to the end of the list
+list.extend() # extend a list with another list
+# nested lists
