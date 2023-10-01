a354a340ea1498df6fba173476ec7e0b3f9cd852
Infernalloo <infernalloo@proton.me>
2023-10-01 19:23:16 +0200

    started day 8

---
+++ 100days/eightDay.py
@@ -0,0 +1,13 @@
+# functions with inputs
+def greet() :
+    print("Hello ")
+    print("World")
+    print("!")
+
+greet()
+
+def greeting(name) :
+ # parameter(argument)
+    print(f"Hello {name}")
+
+greeting("Alex")
