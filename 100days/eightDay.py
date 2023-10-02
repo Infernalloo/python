0345ad92908738c65d5467f715d3f8affc75cba3
Infernalloo <infernalloo@proton.me>
2023-10-02 20:43:29 +0200

    half day 8

--- 100days/eightDay.py
+++ 100days/eightDay.py
@@ -3,11 +3,17 @@ def greet() :
     print("Hello ")
     print("World")
     print("!")
-
 greet()
 
 def greeting(name) :
  # parameter(argument)
     print(f"Hello {name}")
-
 greeting("Alex")
+
+def greet_with(name, location) :
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+# positional arguments
+greet_with("Alex", "Italy")
+# keyword arguments
+greet_with(name="Alex", location="Italy")
