08fb4f0a56ce7d323840cae95305b6244201cebd
Infernalloo <infernalloo@proton.me>
2023-10-27 19:49:02 +0200

    moved 16 day folder into 100 folder

---
+++ 100days/sixtheenDay/main.py
@@ -0,0 +1,19 @@
+# OOP
+# Class --> Object
+# Do differentiate between classes and functions
+# the classes are writen in PascalCase
+
+from turtle import *
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("DarkSeaGreen4")
+timmy.forward(100)
+
+# object.attribute
+my_screen = Screen()
+print(my_screen.canvheight)
+
+# object.method
+my_screen.exitonclick()
