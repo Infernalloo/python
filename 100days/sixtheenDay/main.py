7b64d1bbf7f15f5dd3e09313f8c7c35807420da8
Infernalloo <infernalloo@proton.me>
2023-10-29 19:27:39 +0100

    trying my best for the day 15 project, did day 17 start

--- 100days/sixtheenDay/main.py
+++ 100days/sixtheenDay/main.py
@@ -1,30 +1,14 @@
-# OOP
-# From a Class you can make multiple objects
-# Object = Class
-# car    = CarBlueprint()
-# Object --> Attributes (variables for object)
-# Object --> Methods (functions for object)
-# Do differentiate between classes and functions
-# the classes are writen in PascalCase
-#
-# from turtle import *
-#
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("DarkSeaGreen4")
-# timmy.forward(100)
-#
-# # object.attribute
-# my_screen = Screen()
-# print(my_screen.canvheight)
-#
-# # object.method
-# my_screen.exitonclick()
+user_input = input("What would you like? espresso/latte/cappuccino\n> ").lower()
+power_off = False
+milk = 1000
+water = 1000
+coffee = 500
+money = 0
 
-from prettytable import *
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-print(table)
+if user_input == "off":
+	power_off = True
+elif user_input == "report":
+	print(f"Water: {water}ml")
+	print(f"Milk: {milk}ml")
+	print(f"Coffee: {coffee}g")
+	print(f"Money: {money}$")
---
+++ 100days/sixtheenDay/resources.py
@@ -0,0 +1,15 @@
+def espresso():
+	water = 50
+	coffe = 18
+
+
+def latte():
+	water = 200
+	coffee = 24
+	milk = 150
+
+
+def cappuccino():
+	water = 250
+	coffe = 24
+	milk = 100
