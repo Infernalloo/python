4b689e7265cb1c76ad9ef9ddac7353f7c7c9d89d
Infernalloo <infernalloo@proton.me>
2023-10-31 14:13:43 +0100

    trying to understand the project

--- 100days/fifthteenDay/main.py
+++ 100days/fifthteenDay/main.py
@@ -1,39 +1,23 @@
-from coffe import resources
-from coffe import MENU
-
-selected_coffee = ""
-close_machine = False
+from resources import *
+power_off = False
+profit = 0
 
 
-def coffe_dispenser():
-    global selected_coffee
-    global close_machine
-    current_water = resources["water"]
-    current_milk = resources["milk"]
-    current_coffee = resources["coffee"]
+def check_resources(drink_ingredients):
+    for ingredient in MENU[drink_ingredients]:
+        if drink_ingredients[ingredient] >= resources[ingredient]:
+            print(f"Sorry, we don't have enough {ingredient}")
 
-    user_input = input("What would you like? (Espresso/Latte/Cappuccino)").lower()
-    if user_input == "espresso":
-        selected_coffee = MENU["espresso"]
-        current_water -= MENU["espresso"]["ingredients"]["water"]
-        current_coffee -= MENU["espresso"]["ingredients"]["coffee"]
-    elif user_input == "latte":
-        selected_coffee = MENU["latte"]
-        current_water -= MENU["latte"]["ingredients"]["water"]
-        current_coffee -= MENU["latte"]["ingredients"]["coffee"]
-        current_milk -= MENU["latte"]["ingredients"]["milk"]
-    elif user_input == "cappuccino":
-        selected_coffee = MENU["cappuccino"]
-        current_water -= MENU["cappuccino"]["ingredients"]["water"]
-        current_coffee -= MENU["cappuccino"]["ingredients"]["coffee"]
-        current_milk -= MENU["cappuccino"]["ingredients"]["milk"]
-    elif user_input == "off":
-        close_machine = True
+while not power_off:
+    user_input = input("What do you want to drink? espresso/latte/cappuccino\n> ").lower()
+    if user_input == "off":
+        power_off = True
     elif user_input == "report":
-        print(f"Water: {current_water}")
-        print(f"Milk: {current_milk}")
-        print(f"Coffee: {current_coffee}")
-
-
-while not close_machine:
-    coffe_dispenser()
+        print(f"Water: {resources['water']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Money: {profit}$")
+    else:
+        drink = MENU[user_input]
+        print(drink)
+        check_resources
