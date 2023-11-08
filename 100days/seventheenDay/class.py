744c968ffe1679ea7425fd5470b7e935d327dd12
Infernalloo <infernalloo@proton.me>
2023-11-08 22:23:35 +0100

    restarted day 17

--- 100days/seventheenDay/class.py
+++ 100days/seventheenDay/class.py
@@ -4,16 +4,19 @@ class User:
     # self is the object that
     # is being initialized
     def __init__(self, id, username):
+        # this are the attributes of the class
         self.id = id
         self.username = username
         self.follower = 0
         self.following = 0
-        
+
+    # this is how a method is declared    
     def follow(self, user):
         user.follower += 1
         self.following += 1
 
-
+# here we create an object and 
+# pass the attributes in the User() class
 user_1 = User("001", "alex")
 user_2 = User("002", "jack")
 
