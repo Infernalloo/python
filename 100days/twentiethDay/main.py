e6adceb665860b91dc2afc7c24150493972d75d7
Infernalloo <infernalloo@proton.me>
2023-11-14 08:57:08 +0100

    day 20 made snake body

--- 100days/twentiethDay/main.py
+++ 100days/twentiethDay/main.py
@@ -2,3 +2,14 @@ from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("PySnake by Inferno")
+starting_xcor = [0, -20, -40]
+
+for snake_body in range(3):
+    tim = Turtle(shape="square")
+    tim.color("white")
+    tim.penup()
+    tim.goto(x=starting_xcor[snake_body], y=0)
+
+screen.exitonclick()
