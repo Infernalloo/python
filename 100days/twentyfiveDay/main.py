99e871c6e16eb22bc2f1ee76823e59e0439a4146
Infernalloo <infernalloo@proton.me>
2023-11-25 11:59:55 +0100

    day 25

--- 100days/twentyfiveDay/main.py
+++ 100days/twentyfiveDay/main.py
@@ -1,13 +1,20 @@
-# import csv
-#
-# with open("weather_data.csv") as weather:
-#     data = csv.reader(weather)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-
 import pandas
-data = pandas.read_csv("weather_data.csv")
-print(data)
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+fur_color = data["Primary Fur Color"]
+
+cinnamon = len(data[data["Primary Fur Color"] == "Cinnamon"])
+gray = len(data[data["Primary Fur Color"] == "Gray"])
+black = len(data[data["Primary Fur Color"] == "Black"])
+
+print(cinnamon)
+print(gray)
+print(black)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray, cinnamon, black]
+}
+
+data_frame = pandas.DataFrame(data_dict)
+data_frame.to_csv("Fur Color")
