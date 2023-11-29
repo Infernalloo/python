91e0665dae89eb2b27a5e11f5b2b8d90989dcf74
Infernalloo <infernalloo@proton.me>
2023-11-29 15:21:28 +0100

    redid half of day 25, understood better

--- 100days/25/lesson.py
+++ 100days/25/lesson.py
@@ -18,5 +18,43 @@
 #     print(temperatures)
 
 import pandas
+
+# data frame is equivalent to the full table
+# series is equivalent to column of a table
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+data_temp = data["temp"].to_list()
+# print(data_temp)
+
+avg_temp = data["temp"].mean()
+# print(round(avg_temp))
+
+maximum_temp = data["temp"].max()
+# print(maximum_temp)
+
+# Get data from columns
+# data["temp"]
+# data.temp
+
+# Get data in rows
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# Get a value from a row
+monday = data[data.day == "Monday"]
+# print(monday.condition)
+temp_farh = (monday.temp * (9/5)) + 32
+# print(temp_farh)
+
+# Create a DataFrame from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65],
+}
+new_data = pandas.DataFrame(data_dict)
+print(new_data)
+new_data.to_csv("new_data.csv")
