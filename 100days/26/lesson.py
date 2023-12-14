73ef8b2b4023708a76333060cde30ad801f84bf8
Infernalloo <infernalloo@proton.me>
2023-12-14 15:28:08 +0100

    half day 26

--- 100days/26/lesson.py
+++ 100days/26/lesson.py
@@ -1,3 +1,4 @@
+import random
 # List comprehension
 # new_list = [new_item for item in list]
 
@@ -11,3 +12,18 @@ names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 short_name = [name for name in names if len(name) < 5]
 uppercase_name = [name.upper() for name in names if len(name) >= 5]
 print(uppercase_name)
+
+# Dictionary Comprehension
+# Creating a new dict from a list
+# new_dict = {new_key:new_value for item in list}
+# Creating a new dict form an existing dict
+# new_dict = {new_key:new_value for (key,value) in dict.items()}
+
+# Conditional dictionary comprehension
+# new_dict = {new_key:new_value for (key,value) in dict.items() if test}
+
+students_scores = {student:random.randint(1, 100) for student in names}
+print(students_scores)
+
+passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
+print(passed_students)
